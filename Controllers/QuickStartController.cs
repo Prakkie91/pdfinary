91a777d7b5115296cb720d75732a302eb7cdad2b
Prakkie91 <enricowillemse.was@gmail.com>
2019-05-04 16:36:42 +0200

    Add Quick Start

---
+++ Controllers/QuickStartController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pdfinary.Data;
+using Pdfinary.Models;
+
+namespace Pdfinary.Controllers
+{
+    public class QuickStartController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuickStartController(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            Subscription subscription = _context.Subscriptions.FirstOrDefault(a => a.Id == _subscriptionId);
+
+            ViewBag.Subscription = subscription;
+
+            return View();
+        }
+    }
+}
