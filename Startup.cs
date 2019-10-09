d00f0338b832108261f73e87f37cb6b2d4094728
Prakkie91 <enricowillemse.was@gmail.com>
2019-10-09 11:25:43 +0200

    Update connection string

--- Startup.cs
+++ Startup.cs
@@ -70,6 +70,7 @@ namespace Pdfinary
                 o.Cookie.Name = "Pdfinary.Session";
                 o.IdleTimeout = TimeSpan.FromDays(30);
             });
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
